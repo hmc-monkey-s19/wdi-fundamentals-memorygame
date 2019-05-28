1b87012233ac772f0e785445ae9635191d41cf5c
hmc-monkey-s19 <Ritisharma@MacBook-Pro-172.local>
2019-05-28 07:03:25 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -33,15 +33,26 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 	}
 }
 
-function flipCard(cardId){
+function flipCard(){
+var cardId = this.getAttribute('data-id');
 console.log("User flipped " + cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 cardsInPlay.push(cards[cardId].rank);
+this.setAttribute('src', cards[cardId].cardImage);
 if(cardsInPlay.length === 2) {
 	checkForMatch();
 	}
 }
-flipCard(0);
-flipCard(2);
+
+function createBoard(){
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard());
+		cardElement.appendChild(game-board);
+	}
+}
+createBoard();
 
